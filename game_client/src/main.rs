9aeb238c0400630c6709a68ca607e969a91938fa
rrohrer <ryan.rohrer@gmail.com>
2021-10-19 18:30:58 -0700

    Can jump and collide with the floor

--- game_client/src/main.rs
+++ game_client/src/main.rs
@@ -1,9 +1,23 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+struct Snek {
+    rect: Rect,
+    velocity: Vec2,
+}
+
+struct Barrier {
+    rect: Rect,
+}
+
+#[macroquad::main("SwimmySnek")]
 async fn main() {
     let mut camera_position = vec2(0.,0.);
+    let mut barriers = Vec::new();
+    barriers.push(Barrier{rect: Rect{x: 0., y:-10., w: 200., h: 10.}});
+
+    let mut snek = Snek{rect: Rect{x: 0., y: 0., w: 1., h: 1.}, velocity: vec2(0., 0.)};
     loop {
+        // update logic
         if is_key_pressed(KeyCode::Right) {
             camera_position.x +=  1.;
         }
@@ -16,21 +30,44 @@ async fn main() {
         if is_key_pressed(KeyCode::Down) {
             camera_position.y +=  1.;
         }
+        if is_key_pressed(KeyCode::Space) {
+            snek.velocity.y = 10.;
+        }
 
-        clear_background(RED);
+        // apply gravity to the snek
+        snek.velocity.y -= 9.8 * get_frame_time();
 
+        // move snek based on its velocity, with gravity accelerating down.
+        snek.rect.x = snek.rect.x + snek.velocity.x * get_frame_time();
+        snek.rect.y = snek.rect.y + snek.velocity.y * get_frame_time();
+
+        // did the snek hit the barrier?
+        for b in &barriers {
+            // for some reason macroquad bottom and top are flipped?!?
+            if b.rect.bottom() >= snek.rect.top() {
+                snek.rect.y += b.rect.bottom() - snek.rect.top();
+                snek.velocity.y *= -0.5;
+            }
+        }
+
+        // begin rendering
+        clear_background(LIGHTGRAY);
+
+        // draw all world based things
         set_camera(&Camera2D {
-           zoom: vec2(1. / screen_width() * 2., -1. / screen_height() * 2.),
+           zoom: vec2(1. / screen_width() * 50., 1. / screen_height() * 50.),
             
             target: camera_position,
             ..Default::default()
         });
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(0., 0., 1.0, 1.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        // draw the snek
+        draw_rectangle(snek.rect.x, snek.rect.y, snek.rect.w, snek.rect.h, GREEN);
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        // draw barriers
+        for b in &barriers {
+            draw_rectangle(b.rect.x, b.rect.y, b.rect.w, b.rect.h, BROWN);
+        }
 
         next_frame().await
     }
